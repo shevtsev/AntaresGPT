2a7d602892a53209922159820584acb7594c083b
shevtsev <107879691+shevtsev@users.noreply.github.com>
2023-08-13 20:18:00 +0500

    Add files via upload

--- задача по pyton/задача_по_pyton.py
+++ задача по pyton/задача_по_pyton.py
@@ -18,20 +18,35 @@ class TextToImage:
         self.root = tk.Tk()
         self.root.attributes('-fullscreen', True)
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        keras.mixed_precision.set_global_policy("float32")
 
         #Stable diffusion model
         self.model = keras_cv.models.StableDiffusion(img_width=self.width, img_height=self.height, jit_compile=True)
 
         #Creating an input field
-        self.entry = tk.Entry(self.root, width=50, font=('Times New Roman', 20))
+        self.entry = tk.Entry(self.root,
+                             width=50,
+                            font=('Times New Roman', 20)
+        )
         self.entry.pack(pady=50)
 
         #Creating a button, witch generate image
-        self.image_button = tk.Button(self.root, text="Execute", font=("Times New Roman", 15),background= "bisque", width=20, command=self.print_image)
+        self.image_button = tk.Button(self.root,
+                                     text="Execute",
+                                    font=("Times New Roman", 15),
+                                    bg= "bisque", fg= "darkorange",
+                                   width=20,
+                                  command=self.print_image
+        )
         self.image_button.pack(pady=10)
 
         #Create a close button
-        self.close_button = tk.Button(self.root, text="X", font=("Times New Roman", 13), background= "red", command=self.on_closing)
+        self.close_button = tk.Button(self.root,
+                                     text="X", 
+                                     font=("Times New Roman", 13),
+                                     bg= "red", fg ="white",
+                                    command=self.on_closing
+        )
         self.close_button.place(relx=1, y=0, anchor="ne")
 
         #image settings
@@ -50,17 +65,16 @@ class TextToImage:
             plt.axis("off")
 
     def print_image(self):
-        if self.entry.get().isalpha():
-            images = self.model.text_to_image(self.entry.get(), batch_size=1, num_steps= 15, unconditional_guidance_scale = 7)
-            self.plot_images(images)
-            path = "img1.png"
-            plt.savefig(path, bbox_inches='tight')
-            image = Image.open(path)
-            max_size = (self.width, self.height)
-            image.thumbnail(max_size)
-            photo = ImageTk.PhotoImage(image)
-            self.image_label.config(image=photo)
-            self.image_label.image = photo
+        images = self.model.text_to_image(self.entry.get(), batch_size=1, num_steps= 40)
+        self.plot_images(images)
+        path = "img1.png"
+        plt.savefig(path, bbox_inches='tight')
+        image = Image.open(path)
+        max_size = (self.width, self.height)
+        image.thumbnail(max_size)
+        photo = ImageTk.PhotoImage(image)
+        self.image_label.config(image=photo)
+        self.image_label.image = photo
 
     def run(self):
         self.root.mainloop()
